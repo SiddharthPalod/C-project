de7ec8e6361f195c5b6180aee03807a386abe668
Prateek-Rath <122281363+Prateek-Rath@users.noreply.github.com>
2023-02-18 10:18:50 +0530

    Update Main.c
    we'll decide the help commands after we write functions to do different tasks.

--- Main.c
+++ Main.c
@@ -1,18 +1,42 @@
-#include<stdio.h>
+#include <stdio.h>
 #include<stdlib.h>
-int main(int argc, char *argv[]){
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  return 0;
-  }
+#include <string.h>
+
+struct current_user{
+        char name[20];
+        char contact_no[20];
+        int id;
+};
+
+int main(int argc, char* argv[]){
+        char command[20];
+                struct current_user cu1;
+        if (strcmp(argv[1],"customer") == 0) {
+                printf("Welcome customer, type help to see what u can do.\n, U can also type commands if u already know how to work!\n");
+                printf(">");
+                scanf("%s", command);
+                if (strcmp(command, "help") == 0){
+                        printf("Whatever stuff\n");
+                                }
+                else if(strcmp(command, "login") == 0){
+                        system("/usr/bin/clear");
+                        printf("Name: ");
+                        scanf("%s", cu1.name);
+                }
+
+        }
+        else if (strcmp(argv[1], "owner") == 0){
+                printf("Welcome , type help to see what u can do.\n, U can also type commands if u already know how to work!\n");
+                if (strcmp(command ,"help") == 0){
+                        printf("Whatever stuff\n");
+                }
+        }
+
+        else {
+                printf("Please run code with command './a.out customer' or './a.out owner'");
+                exit(0);
+        }
+
+        return 0;
+}
+
