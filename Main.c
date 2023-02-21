7ca03dac3271b9f61679eb5605e87b5ee6d84299
SiddharthPalod <75942243+SiddharthPalod@users.noreply.github.com>
2023-02-21 16:20:57 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,51 +1,48 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-void Menu_generator(char Restaurant_Name[],int Restaurant_Id,char* Menu_Items[n],int Prices[n],int Stock[n],FILE* menu){
-    no=Restaurant_Id-100
-    for(10<no<=20){
-//        menu= fopen("R11.txt", "w") (has been put in main function)
-        fprintf(menu,"Restaurant_Name: %s\n",Restaurant_Name);
-        fprintf(menu,"Menu_Items:\n");
-        for(int i=0;i<n;i++){
-            fprintf(menu,"%s\n".Menu_Items[i]);
-        };
-        fprintf(menu,"Restaurant_Id: %d\n",Restaurant_Id);
-        fprintf(menu,"Prices:\n");
-        for(int i=0;i<n;i++){
-            fprintf(menu,"%d\n".Price[i]);
-        };
-        fprintf(menu,"Stocks:\n");
-        for(int i=0;i<n;i++){
-            fprintf(menu,"%d\n".Stock[i]);
-        };     
+void Menu_generator(char Restaurant_Name[], int Restaurant_Id, char* Menu_Items[], int Prices[], int Stock[], FILE* menu, int n) {
+    int no = Restaurant_Id - 100;
+    if (no > 10 && no <= 20) {
+        fprintf(menu, "Restaurant_Name: %s\n", Restaurant_Name);
+        fprintf(menu, "Menu_Items:\n");
+        for (int i = 0; i < n; i++) {
+            fprintf(menu, "%s\n", Menu_Items[i]);
         }
+        fprintf(menu, "Restaurant_Id: %d\n", Restaurant_Id);
+        fprintf(menu, "Prices:\n");
+        for (int i = 0; i < n; i++) {
+            fprintf(menu, "%d\n", Prices[i]);
+        }
+        fprintf(menu, "Stocks:\n");
+        for (int i = 0; i < n; i++) {
+            fprintf(menu, "%d\n", Stock[i]);
+        }
+    }
 }
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     //to open all files in database and search for a specific string
     //(let us assume 20 restaurants 10 are premade and max 10 can be created)
-    char fileNames[20][8] = { "Burger King", "KFC", "McDonalds", "Dominoes","Pizza Hut","Juicy","Onesta","Chai Point"."Truffles","Wow Momo","R11", "R12", "R13", "R14","R15","R16","R17","R18"."R19","R20"};
+    char fileNames[20][12] = { "Burger King", "KFC", "McDonalds", "Dominoes", "Pizza Hut", "Juicy", "Onesta", "Chai Point", "Truffles", "Wow Momo", "R11", "R12", "R13", "R14", "R15", "R16", "R17", "R18", "R19", "R20" };
+    char searchStr[] = "search_string";
     for (int i = 0; i < 20; i++) {
         FILE *pFile = fopen(fileNames[i], "r");
-        bool bExists = searchInFile(pFile, searchStr);
-        if (bExists) {
-            printf("%s contains %s\n", fileNames[i], searchStr);
+        if (pFile != NULL) {
+            bool bExists = false;
+            char buffer[100];
+            while (fgets(buffer, sizeof(buffer), pFile) != NULL) {
+                if (strstr(buffer, searchStr) != NULL) {
+                    bExists = true;
+                    break;
+                }
+            }
+            if (bExists) {
+                printf("%s contains %s\n", fileNames[i], searchStr);
+            }
+            fclose(pFile);
         }
-        fclose(pFile);
     }
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  return 0;
-  }
+    return 0;
+}
