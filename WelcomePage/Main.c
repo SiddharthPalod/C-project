6dde6468ae1a7edebb0c2e7c138f4c6b2c8a8130
SoardRaspi <69724218+SoardRaspi@users.noreply.github.com>
2023-02-23 22:54:32 +0530

    Create Main.c

---
+++ WelcomePage/Main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include <string.h>
+
+int main(){
+  printf("\n\t\t\t  |¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯|");
+  printf("\n\t\t\t  |      ------------------------------------------------      |");
+  printf("\n\t\t\t  |      |                                              |      |");
+  printf("\n\t\t\t  |      |                                              |      |");
+  printf("\n\t\t\t  |      |      WELCOME TO FOOD ORDERING SYSTEM 	|      |");
+  printf("\n\t\t\t  |      |                                              |      |");
+  printf("\n\t\t\t  |      |                                              |      |");
+  printf("\n\t\t\t  |      ------------------------------------------------      |");
+  printf("\n\t\t\t  |____________________________________________________________|\n");
+  printf("\n\n\n\n\n");
+  
+  return 0;
+}
