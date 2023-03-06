c4db2221d0c7943f0407a323c2f4e33e0557ef46
sassy2711 <122263904+sassy2711@users.noreply.github.com>
2023-03-07 00:01:18 +0530

    Update num_checker.c

--- num_checker.c
+++ num_checker.c
@@ -2,21 +2,21 @@
 #include <string.h>
 #include <math.h>
 #include"all.h"
-int checkifint(char str[])
-{int c,n;
+int checkifint(char str[])	//function which asks for input again and again unitl user enters a positive integer  
+{int c,n;	//str is a string which has to be shown if the user input is not a positive integer
 	n = 0;
 	do{
 	char x[20];
 	
 	scanf("%s", x);
-	
-	if(x[0] == '-')
+	negati
+	if(x[0] == '-')		//checking if the first character is negtaive
 	{
 	printf("%s\n",str);	
 	}
 	else
 	{
-       	for(int i = 0; i<strlen(x); i++)
+       	for(int i = 0; i<strlen(x); i++)	//loop which uses the ASCII values of the characters to return an integer and a prompt showing whatever string the user wants to be shown otherwise
 	{
 		c = (int)(x[i] - '0');
 		if ((int)(x[i]-'0')<10 && (int)(x[i])>=0)
@@ -25,15 +25,15 @@ int checkifint(char str[])
 		}
 		else
 		{
-			printf("%s\n",str);
+			printf("%s\n",str);	//breaking out of the loop as soon as the first non int character is entered
 			break;
 			
 		}
 	}
 	}
-	}while(c>9 || c<0 );
+	}while(c>9 || c<0 );	//loop keeps running as long as number entered is invalid
 	
-return n;
+return n;	//returns the number entered if the number is valid
 }
 
 
