93bdfe51d234fed3c83629f0c15c50d3aecdacfa
sassy2711 <122263904+sassy2711@users.noreply.github.com>
2023-03-07 23:26:22 +0530

    Update MenuOwner.c

--- Try2/MenuOwner.c
+++ Try2/MenuOwner.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "all.h"
+#include "num_checker.c"
 
 void Menu_generator(char* Menu_Items[], int Prices[],FILE* menu, int no) {
     for (int i = 0; i < no; i++) {
