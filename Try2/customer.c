b9eb5174f99c382a661f0eaeabdfeaf3889bee2a
SoardRaspi <69724218+SoardRaspi@users.noreply.github.com>
2023-03-07 19:24:27 +0530

    Update customer.c

--- Try2/customer.c
+++ Try2/customer.c
@@ -18,7 +18,66 @@
 
 char id[23];
 
+//struct oitem{
+//    char name[20];
+//    int qty;
+//    int price;
+//    struct oitem * next;
+//};
+//
+//struct item{
+//    char name[20];
+//    int no;
+//    int price;
+//    struct item * next;
+//};
+//
+//struct oitem * createoitem(char str[], int qty, struct item * first){
+//    int found = 0;
+//    for(int i=0; first != NULL; first= first->next){
+//        if (strcmp(first->name, str) == 0){
+//            found = 1;
+//            break;
+//        }
+//    }
+//    if (found==1){
+//        struct oitem * it = (struct oitem*)malloc(1 * sizeof(struct oitem));
+//        strcpy(it->name, str);
+//        it->price = first->price;
+//        it->qty = qty;
+//        it->next = NULL;
+//        //printf("%s %d %d", it->name,it->price, it->qty);
+//        return it;
+//    }
+//    else {
+//        return NULL;// item doesn't exist
+//    }
+//
+//}
+//
+//struct oitem * add_oitem(struct oitem * order, struct oitem * new){
+//    if(new == NULL) return NULL;
+//    struct oitem* present = order;
+//    struct oitem* prev  = NULL;
+//    if (present == NULL){
+//        return new;
+//    }
+//    else{
+//        while(present != NULL){
+//            prev = present;
+//            present =  present->next;
+//        }
+//        prev->next = new;
+//        new->next = present;
+//    }
+//    return order;
+//}
+
 int main(int argc, char * argv[]){
+    int count = 0;
+    struct oitem * order = (struct oitem*) malloc(1 * sizeof(struct oitem));
+    order = NULL;
+    
     if (argc == 1 || argc > 2 ){
         printf("Run as ./a.out owner or ./a.out customer\n");
         exit(1);
@@ -450,70 +509,84 @@ int main(int argc, char * argv[]){
         //     // close the socket
         //     close(sockfd);
         
-//        int sockfd, connfd;
-//        struct sockaddr_in servaddr, cli;
-//
-//        // socket create and verification
-//        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-//        if (sockfd == -1) {
-//            printf("socket creation failed...\n");
-//            exit(0);
-//        }
-//        else
-//            printf("Socket successfully created..\n");
-//        bzero(&servaddr, sizeof(servaddr));
-//
-//        // assign IP, PORT
-//        servaddr.sin_family = AF_INET;
-//        servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-//        servaddr.sin_port = htons(PORT);
-//
-//        // connect the client socket to server socket
-//        if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
-//            != 0) {
-//            printf("connection with the server failed...\n");
-//            exit(0);
-//        }
-//        else
-//            printf("connected to the server..\n");
-//
-//        // function for chat
-//        char buff[MAX];
-//        int n;
-//        //    for (;;) {
-//        bzero(buff, sizeof(buff));
-//        printf("start");
-//        //        printf("Enter the string : ");
-//        //        n = 0;
-//        //        while ((buff[n++] = getchar()) != '\n')
-//        //            ;
-//        strcpy(buff, "start");
-//        write(sockfd, buff, sizeof(buff));
-//
-//        bzero(buff, sizeof(buff));
-//        printf("enter restaurant name: ");
-//        n = 0;
-//        while ((buff[n++] = getchar()) != '\n');
-//        //        printf("Enter the string : ");
-//        //        n = 0;
-//        //        while ((buff[n++] = getchar()) != '\n')
-//        //            ;
-//        write(sockfd, buff, sizeof(buff));
-//
-//        for (;;){
-//            bzero(buff, sizeof(buff));
-//            read(sockfd, buff, sizeof(buff));
-//            printf("From Server : %s", buff);
-//
-//            if ((strncmp("confirmed", buff, 4)) == 0) {
-//                printf("Client Exit...\n");
-//                break;
-//            }
-//        }
-//        //    }
-//
-//        // close the socket
-//        close(sockfd);
+        //        int sockfd, connfd;
+        //        struct sockaddr_in servaddr, cli;
+        //
+        //        // socket create and verification
+        //        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        //        if (sockfd == -1) {
+        //            printf("socket creation failed...\n");
+        //            exit(0);
+        //        }
+        //        else
+        //            printf("Socket successfully created..\n");
+        //        bzero(&servaddr, sizeof(servaddr));
+        //
+        //        // assign IP, PORT
+        //        servaddr.sin_family = AF_INET;
+        //        servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+        //        servaddr.sin_port = htons(PORT);
+        //
+        //        // connect the client socket to server socket
+        //        if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
+        //            != 0) {
+        //            printf("connection with the server failed...\n");
+        //            exit(0);
+        //        }
+        //        else
+        //            printf("connected to the server..\n");
+        //
+        //        // function for chat
+        //        char buff[MAX];
+        //        int n;
+        //        //    for (;;) {
+        //        bzero(buff, sizeof(buff));
+        //        printf("start");
+        //        //        printf("Enter the string : ");
+        //        //        n = 0;
+        //        //        while ((buff[n++] = getchar()) != '\n')
+        //        //            ;
+        //        strcpy(buff, "start");
+        //        write(sockfd, buff, sizeof(buff));
+        //
+        //        bzero(buff, sizeof(buff));
+        //        printf("enter restaurant name: ");
+        //        n = 0;
+        //        while ((buff[n++] = getchar()) != '\n');
+        //        //        printf("Enter the string : ");
+        //        //        n = 0;
+        //        //        while ((buff[n++] = getchar()) != '\n')
+        //        //            ;
+        //        write(sockfd, buff, sizeof(buff));
+        //
+        //        for (;;){
+        //            bzero(buff, sizeof(buff));
+        //            read(sockfd, buff, sizeof(buff));
+        //            printf("From Server : %s", buff);
+        //
+        //            if ((strncmp("confirmed", buff, 4)) == 0) {
+        //                printf("Client Exit...\n");
+        //                break;
+        //            }
+        //        }
+        //        //    }
+        //
+        //        // close the socket
+        //        close(sockfd);
+        
+        FILE* common;
+        common = fopen("common.txt", "w");
+        
+        struct oitem * temp;
+        
+        fprintf(common, "%d\n", count);
+        temp = order;
+        for(int i=0; temp != NULL; temp=temp->next){
+            fprintf(common, "%15s %5d %3d %5d\n", temp->name, temp->price, temp->qty, temp->qty * temp->price);
+            
+        }
+        printf("Done writing to file.");
+        fclose(common);
         
         system("gcc client.c -o c.out");
         system("./c.out");
